c39aa0fa20f71047eea8f430a92efde3b17e69d5
Steven Rutherford <steve.rutherford12@gmail.com>
2021-10-02 12:06:05 +1300

    Basic Platformer

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var GAME_SCREEN_WIDTH = 800;
-var GAME_SCREEN_HEIGHT = 600;
+var GAME_SCREEN_WIDTH = 1200;
+var GAME_SCREEN_HEIGHT = 800;
 
 Crafty.init(GAME_SCREEN_WIDTH,GAME_SCREEN_HEIGHT, document.getElementById('game'));
 
