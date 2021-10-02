d03cfdae0af7abd1d532678256f348c24ecde73c
Steven Rutherford <steve.rutherford12@gmail.com>
2021-10-02 17:45:29 +1300

    small fixes.

--- components/sanityzone.js
+++ components/sanityzone.js
@@ -4,19 +4,17 @@ Crafty.c("SanityZone", {
         this.attr({x: 0, y: 0, w: 100, h: 50})
         this.color('black', 0.2);
 
-
         if (this.checkHits("Player")){ //if collide hits with player
             this.bind("HitOn", function(hitData){
                 this.delay(this.sanityDecrement, 500, -1);
                 Crafty("Player").color('yellow');
-        })
+            })
             this.bind("HitOff", function(comp){ //Player leaves zone
                 this.cancelDelay(this.sanityDecrement);
                 Crafty("Player").color('red');
-
-        })
-        }},
-
+            })
+        }
+    },
 
     sanityDecrement: () => {
             currentSanity = Crafty("SanityBar").sanity;
@@ -25,7 +23,4 @@ Crafty.c("SanityZone", {
             Crafty("SanityBar").sanity = currentSanity;
             console.log(currentSanity, "current");
     }
-
-
-
-    })
+})
