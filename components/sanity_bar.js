a9a2befb83f6f685795c6e8dda5859c6c05f9374
Brayden Cooke <87751532+Cookiezznz@users.noreply.github.com>
2021-10-02 16:44:48 +1300

    add/sanityzone
    Reduces sanity while in zone

--- components/sanity_bar.js
+++ components/sanity_bar.js
@@ -4,7 +4,7 @@ const STARTING_SANITY = 50;
 const HIGH_SANITY = 70;
 const MAX_SANITY = 100;
 
-Crafty.c("this", {
+Crafty.c("SanityBar", {
     init: function () {
         this.addComponent("2D, DOM, Color");
         this.attr({x: 0, y: 0, w: 50, h: 180});
@@ -18,12 +18,12 @@ Crafty.c("this", {
         if (MIN_SANITY < value < MAX_SANITY) {
             this.sanity = value;
         }
-        if (getSanity() < LOW_SANITY) {
+        if (this.sanity < LOW_SANITY) {
             if (this.state !== "LOW") {
                 this.state = "LOW";
                 Crafty.trigger("NEW_SANITY_STATE", this.state);
             }
-        } else if (getSanity() > HIGH_SANITY) {
+        } else if (this.sanity > HIGH_SANITY) {
             if (this.state !== "HIGH") {
                 this.state = "HIGH";
                 Crafty.trigger("NEW_SANITY_STATE", this.state);
--- scenes/game.js
+++ scenes/game.js
@@ -1,6 +1,6 @@
 Crafty.defineScene("Game", function() {
 	var player = Crafty.e("Player")
-		.attr({x : 40, y: -100});
+		.attr({x : 0, y: 750});
 
 	var sanityBar = Crafty.e("SanityBar")
 		.attr({x : 1125, y : 100});
@@ -15,7 +15,7 @@ Crafty.defineScene("Game", function() {
 		.attr({x: 700, y: 580, w: 200, h: 20})
 
 	var zone1 = Crafty.e("SanityZone")
-		.attr({x:0, y:700, w: 50, h:50})
+		.attr({x:0, y:770, w: 100, h:10})
 
 	makeCameraTrackEntity(player, 50)
 
