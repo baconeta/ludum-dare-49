5381355cb8ace74f7ec16223cf836a72a6fdb62d
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2021-10-02 21:00:13 +0300

    generalised background setting

--- components/background.js
+++ components/background.js
@@ -1,15 +1,29 @@
 Crafty.c("Background", {
     init: function () {
         this.requires('2D, DOM, Image');
-        this.z = -100000,
-        this.image("assets/images/bg_sad.png", "repeat");
+        this.z = -100000;
     },
-    place: function (x,y,w,h) {
+    place: function (x, y, w, h, lvl) {
         // Add the width, height, and top left co-ordinates for the box/background
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        setBackground.call(this, lvl);
         return this;
     },
 });
+
+function setBackground(lvl) {
+    switch (lvl) {
+        case 1:
+            this.image("assets/images/bg_sad.png", "repeat");
+            break;
+        case 2:
+            this.image("assets/images/bg_sad.png", "repeat"); // change for background angry when ready
+            break;
+        case 3:
+            this.image("assets/images/bg_sad.png", "repeat"); // change for background fear when ready
+            break;
+    }
+}
