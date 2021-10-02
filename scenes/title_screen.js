577e19e2fef0ee075a5b96142432f9424cd4ea74
NikoFarrelly <nickhfarrelly@gmail.com>
2021-10-02 19:17:05 +1300

    Squashed commit of the following:
    commit 6619788b62950414f5bcbbc771abba7992f60eb8
    Merge: 28c26ee 32edecf
    Author: NikoFarrelly <nickhfarrelly@gmail.com>
    Date:   Sat Oct 2 19:16:52 2021 +1300
    
        Merge branch 'master' into feature-add-title-screen
    
    commit 28c26ee4dd35292835b858f0de5ff33415551389
    Author: NikoFarrelly <nickhfarrelly@gmail.com>
    Date:   Sat Oct 2 19:16:43 2021 +1300
    
        added title screen

---
+++ scenes/title_screen.js
@@ -0,0 +1,25 @@
+Crafty.defineScene('Title', function()  {
+    Crafty.background("#AAA");
+    Crafty.e("2D, DOM, Text")
+        .attr({ w: 200, h: 50, x: (GAME_SCREEN_WIDTH/2)-100, y: (GAME_SCREEN_HEIGHT/2)-100 })
+        .text("Click to play 'Emotionally unstable horses who have to go to work on Monday!'")
+        .textFont({ size: '20px', weight: 'bold' })
+        .textAlign("center")
+        .textColor("#111");
+    Crafty.e("2D, DOM, Text")
+        .attr({ w: 200, h: 50, x: 550, y: 700 })
+        .text("Robot Horses made this game!")
+
+    var playGameButton = Crafty.e("2D, DOM, Image, Mouse")
+        .attr({
+            x: 0,
+            y: 0,
+            w: 10000,
+            h: 1000
+        })
+        .bind('Click', function(MouseEvent){
+            Crafty.scene('Game');
+        });
+})
+
+
