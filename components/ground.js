54afe802774bd9fed9746fcb6fdae74edb2e941a
Steven Rutherford <steve.rutherford12@gmail.com>
2021-10-02 17:28:26 +1300

    Walking on platform.

--- components/ground.js
+++ components/ground.js
@@ -1,7 +1,19 @@
 Crafty.c("Ground", {
 	init: function() {
-    this.addComponent("2D, DOM, Color, Collision");
-    this.attr({x: 0, y: 0, w: 200, h: 20})
-    this.color('#070')
+        this.addComponent("2D, DOM, Color, Collision");
+        this.attr({x: 0, y: 0, w: 200, h: 20})
+        this.color('#070')
+        this.leftMovementBoundary = Crafty.e("MovementBoundary");
+        this.rightMovementBoundary = Crafty.e("MovementBoundary");
+  },
+
+  place(x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.leftMovementBoundary.x = this.x - this.leftMovementBoundary.w;
+    this.leftMovementBoundary.y = y - this.h;
+    this.rightMovementBoundary.x = this.x + this.w;
+    this.rightMovementBoundary.y = y - this.h;
   }
 })
--- scenes/game.js
+++ scenes/game.js
@@ -1,21 +1,24 @@
 Crafty.defineScene("Game", function() {
 	var player = Crafty.e("Player")
-		.attr({x : 40, y: 40});
+		.attr({x : 40, y: 440});
 
 	var sanityBar = Crafty.e("SanityBar")
 		.attr({x : 1125, y : 100});
 
 	var ground1 = Crafty.e('Ground')
-		.attr({x: 0, y: 780, w: 200, h: 20})
+		.place(0, 780)
 
 	var ground2 = Crafty.e('Ground')
-		.attr({x: 400, y: 780, w: 200, h: 20})
+		.place(400, 780)
 
 	var ground3 = Crafty.e('Ground')
-		.attr({x: 700, y: 580, w: 200, h: 20})
+		.place(700, 580)
 
 	var enemyJumper = Crafty.e('EnemyJumper')
 		.attr({x: 750, y: 520, w: 20, h: 20})
+
+	var enemyWalker = Crafty.e('EnemyWalker')
+		.attr({x: 420, y: 720, w: 20, h: 20})
 
 	makeCameraTrackEntity(player, 50)
 });
