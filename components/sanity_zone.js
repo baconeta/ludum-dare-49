254ad87e087aa4943a44d6fab007bb4f328529a0
James Coburn <JamesCoburn246@gmail.com>
2021-10-02 21:51:19 +1300

    Make a version of the sanity zone that restores sanity

--- components/sanity_zone.js
+++ components/sanity_zone.js
@@ -1,31 +1,28 @@
 const GAIN_RATE = 3;
 const LOSS_RATE = 3;
+const MODE = { "GAIN": true, "LOSS": false };
 
 Crafty.c("SanityZone", {
     init: function() {
         this.addComponent("2D, DOM, Color, Collision, Delay");
         this.attr({x: 0, y: 0, w: 100, h: 50})
         this.color('black', 0.2);
+        this.mode = MODE.GAIN;
 
         if (this.checkHits("Player")){ //if collide hits with player
-            this.bind("HitOn", function(hitData){
-                this.delay(this.sanityDecrement, 500, -1);
+            this.bind("HitOn", function() {
+                this.delay(this.alterSanity, 500, -1);
                 Crafty("Player").color('yellow');
             })
-            this.bind("HitOff", function(comp){ //Player leaves zone
-                this.cancelDelay(this.sanityDecrement);
+            this.bind("HitOff", function() { //Player leaves zone
+                this.cancelDelay(this.alterSanity);
                 Crafty("Player").color('red');
             })
         }
     },
 
-    sanityIncrement: () => {
-        Crafty("SanityBar").setSanity(Crafty("SanityBar").sanity + GAIN_RATE);
-        console.log(["[Sanity Zone] Current sanity:", Crafty("SanityBar").sanity]);
-    },
-
-    sanityDecrement: () => {
-        Crafty("SanityBar").setSanity(Crafty("SanityBar").sanity - LOSS_RATE);
+    alterSanity: function() {
+        Crafty("SanityBar").setSanity(Crafty("SanityBar").sanity + (this.mode ? GAIN_RATE : -LOSS_RATE));
         console.log(["[Sanity Zone] Current sanity:", Crafty("SanityBar").sanity]);
     },
 })
