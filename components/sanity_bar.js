6888481338e7fe595a3f03e97c95953243156aae
James Coburn <JamesCoburn246@gmail.com>
2021-10-02 14:14:22 +1300

    Add sanity bar. Add to index.html and game.js

---
+++ components/sanity_bar.js
@@ -0,0 +1,15 @@
+const MIN_SANITY = 0;
+const LOW_SANITY = 30;
+const STARTING_SANITY = 50;
+const HIGH_SANITY = 70;
+const MAX_SANITY = 100;
+
+Crafty.c("SanityBar", {
+    init: function () {
+        this.addComponent("2D, DOM, Color");
+        this.attr({x: 0, y: 0, w: 50, h: 180});
+        this.color('#55ff00');
+        this.sanity = STARTING_SANITY;
+        this.state = "MED";
+    }
+})
--- scenes/game.js
+++ scenes/game.js
@@ -1,6 +1,30 @@
+var sanityBar;
+
+const setSanity = (value) => {
+	if (MIN_SANITY < value < MAX_SANITY) {
+		sanityBar.sanity = value;
+	}
+	if (getSanity() < LOW_SANITY) {
+		sanityBar.state = "LOW";
+	} else if (getSanity() < HIGH_SANITY) {
+		sanityBar.state = "NORMAL";
+	} else {
+		sanityBar.state = "HIGH";
+	}
+};
+const getSanity = () => {
+	return sanityBar.sanity
+};
+const getState = () => {
+	return sanityBar.state
+}
+
 Crafty.defineScene("Game", function() {
 	var player = Crafty.e("Player")
 		.attr({x : 40, y: 40});
+
+	var sanityBar = Crafty.e("SanityBar")
+		.attr({x : 1125, y : 100});
 
 	var ground1 = Crafty.e('Ground')
 		.attr({x: 0, y: 780, w: 200, h: 20})
