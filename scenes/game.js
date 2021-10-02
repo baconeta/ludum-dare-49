ba569b34c00180bd9ea8cc85fdd2d1aab47121bc
James Coburn <JamesCoburn246@gmail.com>
2021-10-02 22:08:39 +1300

    Update the testing level to have two sanity zones and to use consts

--- scenes/game.js
+++ scenes/game.js
@@ -1,33 +1,35 @@
 Crafty.defineScene("Game", function() {
-	var ground2 = Crafty.e('UnstableDroppingGround')
+	const unstableGround2 = Crafty.e('UnstableDroppingGround')
 		.attr({x: -400, y: 780, w: 200, h: 20})
 
-	var player = Crafty.e("Player")
+	const player = Crafty.e("Player")
 		.attr({x : 40, y: 440});
 
 	const hud = Crafty.e("HUD");
 
-	var ground1 = Crafty.e('Ground')
+	const ground1 = Crafty.e('Ground')
 		.place(0, 780)
 
-	var ground3 = Crafty.e('Ground')
+	const ground3 = Crafty.e('Ground')
 		.place(300, 680)
 
-	var ground3 = Crafty.e('Ground')
+	const ground4 = Crafty.e('Ground')
 		.place(700, 580)
 
-	var enemyJumper = Crafty.e('EnemyJumper')
+	const enemyJumper = Crafty.e('EnemyJumper')
 		.attr({x: 750, y: 520, w: 20, h: 20})
-		
-	var zone1 = Crafty.e("SanityZone")
-		.attr({x:0, y:770, w: 100, h:10})
 
-
-	var enemyWalker = Crafty.e('EnemyWalker')
+	const enemyWalker = Crafty.e('EnemyWalker')
 		.attr({x: 340, y: 670, w: 20, h: 20})
 
-	const sanityBooster = Crafty.e('SanityBooster')
-		.attr({x: 800, y: 570})
+	const sanityBooster = Crafty.e("SanityZone")
+		.attr({x:-50, y:720, mode: MODE.LOSS})
+
+	const sanityDropper = Crafty.e('SanityZone')
+		.attr({x: 100, y: 720, mode: MODE.GAIN})
+
+	const sanityWall = Crafty.e("SanityWall")
+		.attr({x: 750, y: 520})
 
 	// Example of the sanity dropper.
 	/*
