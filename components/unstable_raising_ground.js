1d7f00b3dc8e0206cd14b5afda799f3599153164
NikoFarrelly <nickhfarrelly@gmail.com>
2021-10-03 18:08:56 +1300

    Squashed commit of the following:
    commit 4ee497771849888afacfb52f68ff9182bf8d037c
    Author: NikoFarrelly <nickhfarrelly@gmail.com>
    Date:   Sun Oct 3 18:08:25 2021 +1300
    
        implemented platform raise speed/height def

--- components/unstable_raising_ground.js
+++ components/unstable_raising_ground.js
@@ -1,27 +1,28 @@
 var COLOR = 'red';
-var HEIGHT_BEFORE_DROP = -500; // -y is 'north'/'up'
+var DEFAULT_RAISE_HEIGHT = -500; // -y is 'north'/'up'
+var DEFAULT_RAISE_SPEED = -100;
 
 Crafty.c("UnstableRaisingGround", {
     init: function () {
         this.addComponent("2D, DOM, Gravity, Motion, pf_sad_up");
         this.attr({x: 0, y: 0, w: 101, h: 160, ay: 0})
-
+        this.raiseHeight = DEFAULT_RAISE_HEIGHT;
+        this.raiseSpeed = DEFAULT_RAISE_SPEED;
         this.collisionTop = Crafty.e("PlatformTop")
         this.attach(this.collisionTop);
         this.collisionTop.w = 70;
-        this.bind('LandedOnDecayGround', () => {
-            this.ay = -100
-        })
+        this.bind('LandedOnDecayGround', () => this.ay = this.raiseSpeed)
         this.originY = 0;
         this.returnToOrigin = false;
+
         this.bind('Move', (e) => {
             // setup originY
             if (this.originY === 0 && this.y !== 0) this.originY = this.y;
 
             // move component back down
-            if (this.y < HEIGHT_BEFORE_DROP && this.y !== 0 && !this.returnToOrigin) {
+            if (this.y < this.raiseHeight && this.y !== 0 && !this.returnToOrigin) {
                 this.resetMotion();
-                this.ay = 100;
+                this.ay = -this.raiseSpeed;
                 this.returnToOrigin = true;
             }
 
@@ -34,5 +35,10 @@ Crafty.c("UnstableRaisingGround", {
                 this.move('n', this.y - this.originY)
             }
         })
-    }
+    },
+    setRaise: function (raiseHeight, raiseSpeed) {
+        this.raiseHeight = raiseHeight;
+        this.raiseSpeed = raiseSpeed;
+        return this;
+    },
 })
