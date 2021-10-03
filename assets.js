1330f918fc251630234b8ff9519fab5647c97e7d
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2021-10-04 01:55:23 +0300

    add creature animations

--- assets.js
+++ assets.js
@@ -42,11 +42,11 @@ var gameAssets = {
             "tile": 268,
             "tileh": 268,
             "map": {"music": [0, 0]}
-        },"assets/images/bush.png": {
+        }, "assets/images/bush.png": {
             "tile": 86,
             "tileh": 66,
             "map": {"bush_berries": [0, 0]}
-        },"assets/images/spikes.png": {
+        }, "assets/images/spikes.png": {
             "tile": 86,
             "tileh": 66,
             "map": {"bush_spiky": [0, 0]}
@@ -65,7 +65,7 @@ var gameAssets = {
             "tile": 69,
             "tileh": 209,
             "map": {"sprite_idle_left": [0, 0]}
-        },"assets/images/idle_right.png": {
+        }, "assets/images/idle_right.png": {
             "tile": 69,
             "tileh": 209,
             "map": {"sprite_idle_right": [0, 0]}
@@ -74,10 +74,18 @@ var gameAssets = {
             "tile": 135,
             "tileh": 186,
             "map": {"sprite_jump_left": [0, 0]}
-        },"assets/images/elfsprite_jump_right.png": {
+        }, "assets/images/elfsprite_jump_right.png": {
             "tile": 135,
             "tileh": 186,
             "map": {"sprite_jump_right": [0, 0]}
+        }, "assets/images/enemy_facing_right.png": {
+            "tile": 69,
+            "tileh": 106,
+            "map": {"enemy_facing_right": [0, 0]}
+        }, "assets/images/enemy_facing_left.png": {
+            "tile": 69,
+            "tileh": 106,
+            "map": {"enemy_facing_left": [0, 0]}
         },
     }
 };
