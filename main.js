8479a414cfd9aa6cf95debc838658461b4f44e1f
NikoFarrelly <nickhfarrelly@gmail.com>
2021-10-03 15:51:54 +1300

    Squashed commit of the following:
    commit 810ccfdc59c32615c6dcd1549ac06f711fa48a2a
    Merge: 911419e 358767a
    Author: NikoFarrelly <nickhfarrelly@gmail.com>
    Date:   Sun Oct 3 15:51:41 2021 +1300
    
        update from master
    
    commit 911419edbaa7c5d4da36939f89e473d75f2d8eb0
    Author: NikoFarrelly <nickhfarrelly@gmail.com>
    Date:   Sun Oct 3 15:50:17 2021 +1300
    
        updated audio sprite, turned music to default off
    
    commit 2a8f75133f1fbffee5948d2560ff4777344fab93
    Author: NikoFarrelly <nickhfarrelly@gmail.com>
    Date:   Sun Oct 3 15:48:13 2021 +1300
    
        update from master
    
    commit 8d4a4a567139ba6d367f46184d1cfedc864282cb
    Author: NikoFarrelly <nickhfarrelly@gmail.com>
    Date:   Sun Oct 3 15:39:58 2021 +1300
    
        implemented audio & audio sprite

--- main.js
+++ main.js
@@ -3,8 +3,18 @@ var GAME_SCREEN_HEIGHT = 800;
 
 var DEBUG = false;
 
+// needs to be above game init, as each level needs the AudioController
+var audioController = Crafty.e("AudioController");
 Crafty.init(GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT, document.getElementById('game'));
 
 Crafty.scene("Title");
-
+// controllers
 const levelController = Crafty.e("LevelController");
+
+// audio setup
+var BASE_SOUND_URL = "assets/sounds/"
+// add music tracks here
+audioController.loadTrack('theme', [`${BASE_SOUND_URL}theme.ogg`])
+audioController.loadTrack('sadness',[`${BASE_SOUND_URL}sadness.ogg`])
+
+
