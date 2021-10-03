576d2a929a386da59279bb50e6e185e47ae8a7ff
James Coburn <JamesCoburn246@gmail.com>
2021-10-03 13:47:11 +1300

    Remove default x,y coords. Change colour for now

--- components/consumables/sanity_boost_prop.js
+++ components/consumables/sanity_boost_prop.js
@@ -1,8 +1,8 @@
 Crafty.c("SanityBooster", {
     init: function() {
         this.addComponent("2D, DOM, Color, Gravity");
-        this.attr({x: 800, y: 570, w: 50, h: 50});
-        this.color('#f3e903');
+        this.attr({x: 0, y: 0, w: 50, h: 50});
+        this.color('#F303CBFF');
         // TODO Update with pull request.
         this.gravity("Ground");
     }
