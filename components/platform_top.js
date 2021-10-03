8ab0b14320ce28f600508cbbdbbf5b1e784de136
Tomas Morton <tomas.morton@hotmail.co.nz>
2021-10-03 19:12:55 -0400

    Publish LiftedOffDecayGround event up to PlatformTop parent

--- components/platform_top.js
+++ components/platform_top.js
@@ -1,13 +1,17 @@
 Crafty.c("PlatformTop", {
-    init: function() {
+    init: function () {
         this.addComponent("2D, DOM, Collision, Solid, Color");
         if (DEBUG) {
             this.color("red");
         }
         this.attr({x: 20, y: 7, w: 150, h: 7})
+
         // Pass any events up to parent that we need.
         this.bind('LandedOnDecayGround', (e) => {
             this._parent.trigger('LandedOnDecayGround', e)
+        });
+        this.bind('LiftedOffDecayGround', (e) => {
+            this._parent.trigger('LiftedOffDecayGround', e)
         });
     }
 })
