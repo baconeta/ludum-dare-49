83dd0654050f36266060735eb839f07a46e634b6
James Coburn <JamesCoburn246@gmail.com>
2021-10-03 13:51:46 +1300

    Remove alpha value

--- components/consumables/sanity_boost_prop.js
+++ components/consumables/sanity_boost_prop.js
@@ -2,7 +2,7 @@ Crafty.c("SanityBooster", {
     init: function() {
         this.addComponent("2D, DOM, Color, Gravity");
         this.attr({x: 0, y: 0, w: 50, h: 50});
-        this.color('#F303CBFF');
+        this.color('#F303CB');
         // TODO Update with pull request.
         this.gravity("Ground");
     }
