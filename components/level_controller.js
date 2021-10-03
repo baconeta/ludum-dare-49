640edf926f3d7253b8ad90fde2cb765166552732
Tomas Morton <tomas.morton@hotmail.co.nz>
2021-10-02 20:47:38 -0400

    Add level controller component

---
+++ components/level_controller.js
@@ -0,0 +1,42 @@
+Crafty.c("LevelController", {
+    init: function () {
+        this.addComponent("Persist, 2D, DOM");
+        this.level = 0;
+        this.totalLevels = 5;
+        this.nextLevelLoading = false;
+
+        this.bind("NextLevel", () => {
+            if (this.canChangeLevel()) {
+                this.startLoading();
+                setTimeout(this.changeLevel, 500);
+            }
+        });
+
+        this.bind("ResetLevel", () => {
+            if (!this.nextLevelLoading) {
+                this.startLoading();
+                setTimeout(this.resetLevel, 500);
+            }
+        });
+
+        this.startLoading = () => {
+            Crafty.scene("Loading");
+            this.nextLevelLoading = true;
+        };
+
+        this.canChangeLevel = () => {
+            return !this.nextLevelLoading && this.level <= this.totalLevels;
+        };
+
+        this.changeLevel = () => {
+            this.level++;
+            Crafty.scene(`Level${this.level}`);
+            this.nextLevelLoading = false;
+        };
+
+        this.resetLevel = () => {
+            Crafty.scene(`Level${this.level}`);
+            this.nextLevelLoading = false;
+        };
+    }
+});
