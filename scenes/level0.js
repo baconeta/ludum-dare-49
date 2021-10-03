c003bbbeb25117b5892a95b23b90f6c8915aab77
Steven Rutherford <steve.rutherford12@gmail.com>
2021-10-03 16:46:29 +1300

    Allow enemy walkers to run on moving platforms

--- scenes/level0.js
+++ scenes/level0.js
@@ -57,6 +57,8 @@ Crafty.defineScene('Level0', function () {
 		.attr({x: 750, y: 520, w: 20, h: 20})
 	const enemyWalker = Crafty.e('EnemyWalker')
 		.attr({x: 340, y: 670, w: 20, h: 20})
+	const enemyWalker2 = Crafty.e('EnemyWalker')
+		.attr({x: -550, y: 400, w: 20, h: 20})
 
 	// misc
 	const player = Crafty.e('Player')
