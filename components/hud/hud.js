29dee1e9e2e7eca42ce4680858f1156a7314b576
Tomas Morton <tomas.morton@hotmail.co.nz>
2021-10-03 15:09:50 -0400

    Format

--- components/hud/hud.js
+++ components/hud/hud.js
@@ -8,12 +8,12 @@ Crafty.c("HUD", {
         this.z = 1500;
         this.color("black");
         // TODO Replace with imagery.
-        this.alpha = 0.00
+        this.alpha = 0.00;
 
         const offsetLeft = (entity, amount) => entity.x = this.x + amount;
-        const offsetRight = (entity, amount) => entity.x = this.x + Crafty.viewport.width - amount
-        const offsetTop = (entity, amount) => entity.y = this.y + amount
-        const offsetBottom = (entity, amount) => entity.y = this.y + Crafty.viewport.height - amount
+        const offsetRight = (entity, amount) => entity.x = this.x + Crafty.viewport.width - amount;
+        const offsetTop = (entity, amount) => entity.y = this.y + amount;
+        const offsetBottom = (entity, amount) => entity.y = this.y + Crafty.viewport.height - amount;
 
         const sanityBar = Crafty.e("SanityBar");
         offsetRight(sanityBar, 50);
@@ -25,7 +25,7 @@ Crafty.c("HUD", {
         offsetBottom(itemSlot, 400);
         this.attach(itemSlot);
 
-        const music = Crafty.e('Music')
+        const music = Crafty.e("Music");
         offsetTop(music, 10);
         offsetLeft(music, 10);
         this.attach(music);
@@ -36,7 +36,7 @@ Crafty.c("HUD", {
             }
         });
 
-        this.bind("ViewportScroll", function (e) {
+        this.bind("ViewportScroll", function () {
             this.x = -Crafty.viewport.x;
             this.y = -Crafty.viewport.y;
         });
