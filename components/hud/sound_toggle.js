ff8e461ca09fa7d4d27ce27dafa138a5536774b3
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2021-10-04 22:58:16 +0300

    Update sound_toggle.js

--- components/hud/sound_toggle.js
+++ components/hud/sound_toggle.js
@@ -3,6 +3,11 @@ Crafty.c('Music', {
         this.addComponent("2D, DOM, Color, Image, Mouse, music");
         this.attr({x: 0, y: 0, z: 1500, w: 50, h: 50});
         this.color('#333', 0.25);
+        if (audioController.isMuted) {
+            this.image("assets/images/buttons/music_mute.png")
+        } else {
+            this.image("assets/images/buttons/music_playing.png")
+        }
 
         this.bind('Click', () => {
             audioController.muteToggle()
