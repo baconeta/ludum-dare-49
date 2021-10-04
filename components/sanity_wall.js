fb5a826205a367f8865b1539c8cb0d307f8390b1
James Coburn <JamesCoburn246@gmail.com>
2021-10-04 23:54:22 +1300

    Add logic to change sprites based on level

--- components/sanity_wall.js
+++ components/sanity_wall.js
@@ -2,25 +2,13 @@ Crafty.c("SanityWall", {
     init: function () {
         this.addComponent("2D, DOM, tree, tree_alive_sad");
         this.attr({w: 74, h: 284});
-        this.blocking = true;
+        this.alive = true;
 
         Crafty.bind("NEW_SANITY_STATE", (newState) => {
             if (newState === STABILITY.HIGH || newState === STABILITY.MEDIUM) {
-                if (this.blocking === false) {
-                    this.addComponent("tree");
-                    this.addComponent("tree_alive_sad");
-                    this.removeComponent("tree_dead_sad");
-                    this.y -= 124;
-                    this.blocking = true;
-                }
+                this.makeAlive();
             } else if (newState === STABILITY.LOW) {
-                if (this.blocking === true) {
-                    this.removeComponent("tree");
-                    this.removeComponent("tree_alive_sad");
-                    this.addComponent("tree_dead_sad");
-                    this.y += 124;
-                    this.blocking = false;
-                }
+                this.makeDead();
             }
         });
     },
@@ -28,5 +16,45 @@ Crafty.c("SanityWall", {
     place(x, y) {
         this.x = x;
         this.y = y;
-    }
-})
+    },
+
+    makeAlive() {
+        if (this.alive === true) {
+            return;
+        }
+        switch (Crafty("LevelController").level) {
+            case LEVELS.SADNESS:
+                this.addComponent("tree");
+                this.addComponent("tree_alive_sad");
+                this.removeComponent("tree_dead_sad");
+                this.y -= 124;
+                this.alive = true;
+                break;
+            case LEVELS.ANGER:
+                break;
+            case LEVELS.FEAR:
+            default:
+                break;
+        }
+    },
+
+    makeDead() {
+        if (this.alive === false) {
+            return;
+        }
+        switch (Crafty("LevelController").level) {
+            case LEVELS.SADNESS:
+                this.removeComponent("tree");
+                this.removeComponent("tree_alive_sad");
+                this.addComponent("tree_dead_sad");
+                this.y += 124;
+                this.alive = false;
+                break;
+            case LEVELS.ANGER:
+                break;
+            case LEVELS.FEAR:
+            default:
+                break;
+        }
+    },
+});
