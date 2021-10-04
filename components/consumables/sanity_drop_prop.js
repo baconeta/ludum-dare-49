d53b3bc9a982a5447cce2b858bebdf8ac7a4c537
Steven Rutherford <steve.rutherford12@gmail.com>
2021-10-04 22:06:21 +1300

    Vials

--- components/consumables/sanity_drop_prop.js
+++ components/consumables/sanity_drop_prop.js
@@ -1,7 +1,7 @@
 Crafty.c("SanityDropper", {
     init: function() {
         this.addComponent("2D, DOM, vial_red");
-        this.attr({x: 0, y: 0, w: 50, h: 50});
+        this.attr({x: 0, y: 0, w: 220/4, h: 259/4});
         // TODO Update with pull request.
         // this.gravity("Ground");
     }
